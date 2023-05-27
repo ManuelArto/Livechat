a54ca97a5887e9bbc14130b5f26d7f74e2c90898
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-05-27 13:49:35 +0200

    extract error handlers

---
+++ SERVER/app/error_handlers.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+def add_exception_handlers(app: FastAPI):
+	"""
+	Adds exception handlers to the FastAPI app instance.
+	
+	:param app: A FastAPI instance to add exception handlers to.
+	:return: None
+	"""
+	
+	@app.exception_handler(RequestValidationError)
+	async def validation_exception_handler(request: Request, exc: RequestValidationError):
+		return JSONResponse(
+			status_code=422,
+			content={"error": exc.errors()},
+		)
+	
+	@app.exception_handler(HTTPException)
+	async def validation_exception_handler(request: Request, exc: HTTPException):
+		return JSONResponse(
+			status_code=exc.status_code,
+			content={"error": exc.detail},
+		)
+	
+	@app.exception_handler(Exception)
+	async def validation_exception_handler(request: Request, exc: Exception):
+		return JSONResponse(
+			status_code=500,
+			content={"error": f"Some internal error occured: {str(exc)}"},
+		)
--- SERVER/app/main.py
+++ SERVER/app/main.py
@@ -1,7 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.db import db
@@ -31,29 +29,15 @@ app.mount('/', app=chat.sio_app)
 
 
 # Error handlers
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=422,
-        content={"error": exc.errors()},
-    )
-@app.exception_handler(HTTPException)
-async def validation_exception_handler(request: Request, exc: HTTPException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"error": exc.detail},
-    )
-@app.exception_handler(Exception)
-async def validation_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=500,
-        content={"error": f"Some internal errore occured: {str(exc)}"},
-    )
+from app.error_handlers import add_exception_handlers
+add_exception_handlers(app)
 
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        reload=settings.DEBUG_MODE,
+        log_level="error",
+        use_colors=True,
+        reload=True,
     )
