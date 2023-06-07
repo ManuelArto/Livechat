2b9a11eac9a6704b57655339abb1c4e7b93c422f
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-06-07 16:25:52 +0200

    AuthScreen al centro non ancora scrollable

--- APP/livechat/lib/screens/auth/auth_screen.dart
+++ APP/livechat/lib/screens/auth/auth_screen.dart
@@ -10,39 +10,50 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           const GradienBackGround(double.infinity),
-          SingleChildScrollView(
-            child: SafeArea(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: screenSize.height * 0.02),
-                    child: Image.asset('assets/images/logo_nobg.png',
-                        height: screenSize.height * 0.12),
+          CustomScrollView(
+            scrollDirection: Axis.vertical,
+            slivers: [
+              SliverFillRemaining(
+                child: SafeArea(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: screenSize.height * 0.02),
+                        child: Image.asset('assets/images/logo_nobg.png',
+                            height: screenSize.height * 0.12),
+                      ),
+                      Text(
+                        'Livechat',
+                        style: TextStyle(
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .bodyLarge
+                              ?.color,
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      // add a container with maximum width
+                      Center(
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(
+                              maxWidth: screenSize.width > screenSize.height
+                                  ? screenSize.width * 0.4
+                                  : double.infinity),
+                          child: const AuthForm(),
+                        ),
+                      )
+                    ],
                   ),
-                  Text(
-                    'Livechat',
-                    style: TextStyle(
-                      color: Theme.of(context).primaryTextTheme.bodyLarge?.color,
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  // add a container with maximum width
-                  Center(
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints(
-                          maxWidth: screenSize.width > screenSize.height
-                              ? screenSize.width * 0.4
-                              : double.infinity),
-                      child: const AuthForm(),
-                    ),
-                  )
-                ],
+                ),
               ),
-            ),
+            ],
           ),
         ],
       ),
