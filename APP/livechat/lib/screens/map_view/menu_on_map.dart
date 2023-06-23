25419653c9f507f0b8d1401ff6f71c794d561f8c
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-06-23 16:57:42 +0200

    add search bar to menu on map

--- APP/livechat/lib/screens/map_view/menu_on_map.dart
+++ APP/livechat/lib/screens/map_view/menu_on_map.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import '../../models/friend.dart';
 import 'map_view_screen.dart';
 
 class MenuOnMap extends StatefulWidget {
   const MenuOnMap({
-      super.key,
-      required this.friendsOnMap,
-      required this.scrollController,
-      required MapController mapController, 
-    }) : _mapController = mapController;
+    super.key,
+    required this.friendsOnMap,
+    required this.scrollController,
+    required MapController mapController,
+  }) : _mapController = mapController;
 
   final MapController _mapController;
   final List<Friends> friendsOnMap;
@@ -19,6 +20,17 @@ class MenuOnMap extends StatefulWidget {
 }
 
 class _MenuOnMapState extends State<MenuOnMap> {
+  String _searchingString = "";
+  final TextEditingController _searchController = TextEditingController();
+  late List<Friend> friendsFiltered;
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(
+      () => setState(() => _searchingString = _searchController.text),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -36,35 +48,118 @@ class _MenuOnMapState extends State<MenuOnMap> {
             ),
           ),
           const Divider(thickness: 1),
-          Expanded(
-            child: ListView.separated(
-              separatorBuilder: (context, index) => const Divider(thickness: 1),
-              controller: widget.scrollController,
-              itemCount: widget.friendsOnMap.length,
-              itemBuilder: (context, index) {
-                Friends friend = widget.friendsOnMap[index];
-                return ListTile(
-                  title: Text(friend.name),
-                  subtitle: Text('Steps: ${friend.steps}'),
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(friend.img),
-                  ),
-                  onTap: () {
-                    setState(() {
-                      final LatLng friendLocation = LatLng(
-                        friend.latitude,
-                        friend.longitude,
-                      );
-                      widget._mapController.move(friendLocation, 13.0);
-                    });
-                    Navigator.of(context).pop();
-                  },
-                );
-              },
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: SearchBar(
+              hintText: "Add or find a friend",
+              shape: MaterialStateProperty.all(
+                const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+              textStyle: MaterialStateProperty.all(
+                const TextStyle(backgroundColor: Colors.transparent),
+              ),
+              controller: _searchController,
+              leading: const Icon(Icons.search_rounded),
+              trailing: _searchController.text.isNotEmpty
+                  ? [
+                      IconButton(
+                          icon: const Icon(Icons.clear_rounded),
+                          onPressed: _searchController.clear),
+                    ]
+                  : null,
             ),
           ),
+          if (_searchingString.isEmpty) ...[
+            Expanded(
+              child: ListView.separated(
+                separatorBuilder: (context, index) =>
+                    const Divider(thickness: 1),
+                controller: widget.scrollController,
+                itemCount: widget.friendsOnMap.length,
+                itemBuilder: (context, index) {
+                  Friends friend = widget.friendsOnMap[index];
+                  return ListTile(
+                    title: Text(friend.name),
+                    subtitle: Text('Steps: ${friend.steps}'),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(friend.img),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        final LatLng friendLocation = LatLng(
+                          friend.latitude,
+                          friend.longitude,
+                        );
+                        widget._mapController.move(friendLocation, 13.0);
+                      });
+                      Navigator.of(context).pop();
+                    },
+                  );
+                },
+              ),
+            ),
+          ] else
+            Expanded(child: FindUsersTab(_searchingString, widget.friendsOnMap, widget._mapController))
         ],
       ),
+    );
+  }
+}
+
+class FindUsersTab extends StatefulWidget {
+  const FindUsersTab(String searchingString, this.friendsOnMap, this._mapController, {super.key})
+      : _searchingString = searchingString;
+
+  final String _searchingString;
+  final List<Friends> friendsOnMap;
+  final MapController _mapController;
+
+  @override
+  State<FindUsersTab> createState() => _FindUsersTabState();
+}
+
+class _FindUsersTabState extends State<FindUsersTab> {
+  @override
+  Widget build(BuildContext context) {
+    List<Friends> friendsFiltered = widget.friendsOnMap
+        .where(
+          (friends) => friends.name
+              .toLowerCase()
+              .contains(widget._searchingString.toLowerCase()),
+        )
+        .toList();
+
+    return CustomScrollView(
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.only(top: 12.0),
+          sliver: SliverList.separated(
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: friendsFiltered.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(friendsFiltered[index].name),
+                subtitle: Text('Steps: ${friendsFiltered[index].steps}'),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(friendsFiltered[index].img),
+                ),
+                onTap: () {
+                  setState(() {
+                    final LatLng friendLocation = LatLng(
+                      friendsFiltered[index].latitude,
+                      friendsFiltered[index].longitude,
+                    );
+                    widget._mapController.move(friendLocation, 13.0);
+                  });
+                  Navigator.of(context).pop();
+                },
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
