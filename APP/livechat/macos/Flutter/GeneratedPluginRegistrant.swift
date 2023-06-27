c99d6a194c0d777adca2bf849ef92056bd1c1663
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-06-27 23:51:37 +0200

    Aggiunta roba compilata

--- APP/livechat/macos/Flutter/GeneratedPluginRegistrant.swift
+++ APP/livechat/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import isar_flutter_libs
 import path_provider_foundation
 import share_plus_macos
@@ -13,6 +14,7 @@ import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
