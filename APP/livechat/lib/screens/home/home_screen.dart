0480694c0e10b871b35c4d79505d7b6730a60c25
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-07-01 20:12:08 +0200

    Immagini più carine in home

--- APP/livechat/lib/screens/home/home_screen.dart
+++ APP/livechat/lib/screens/home/home_screen.dart
@@ -18,8 +18,7 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>
-    with AutomaticKeepAliveClientMixin {
+class _HomeScreenState extends State<HomeScreen> with AutomaticKeepAliveClientMixin {
       
   Route<dynamic>? _buildRoute(settings) {
     switch (settings.name) {
@@ -53,21 +52,19 @@ class _HomeScreenState extends State<HomeScreen>
                 children: [
                   Expanded(
                     child: CardHome(
-                      imgURL:
-                          "https://cdn.pixabay.com/photo/2017/06/10/07/21/chat-2389223_960_720.png",
+                      imgPath: "home/chat_icon.png",
                       nameCard: "Chat",
                       description:
-                          "Start private conversations or join group chats with your friends",
+                          "Start private conversations with your friends",
                     ),
                   ),
                   SizedBox(width: 8),
                   Expanded(
                     child: CardHome(
-                      imgURL:
-                          "https://img.lovepik.com/free-png/20210919/lovepik-map-location-sign-number-png-image_400315655_wh1200.png",
+                      imgPath: "home/map_icon.png",
                       nameCard: "Map",
                       description:
-                          "Explore the real-time locations of your friends on a map when they are online",
+                          "Explore the real-time locations of your friends",
                     ),
                   ),
                 ],
@@ -79,8 +76,7 @@ class _HomeScreenState extends State<HomeScreen>
                 children: [
                   Expanded(
                     child: CardHome(
-                      imgURL:
-                          "https://media.istockphoto.com/id/1064128676/it/vettoriale/icona-del-segno-del-gruppo-di-persone-simbolo-di-condivisione-bottone-navigazione-nel.jpg?s=1024x1024&w=is&k=20&c=IRexgR3l1fo0NmP8mZV9sdNDHbw-hHyPmi3AHJ4gOm4=",
+                      imgPath: "home/friends_icon.png",
                       nameCard: "Friends",
                       description:
                           "Connect and build new friendships with people. Send and view friend requests",
@@ -89,11 +85,10 @@ class _HomeScreenState extends State<HomeScreen>
                   SizedBox(width: 8),
                   Expanded(
                     child: CardHome(
-                      imgURL:
-                          "https://static.vecteezy.com/system/resources/previews/022/876/363/non_2x/social-media-user-3d-cartoon-illustration-speech-bubble-with-internet-icon-png.png",
+                      imgPath: "home/profile_icon.png",
                       nameCard: "Profile",
                       description:
-                          "Customize your profile. View and update your personal information and edit the theme",
+                          "View your personal information and customize your theme settings and more",
                     ),
                   ),
                 ],
