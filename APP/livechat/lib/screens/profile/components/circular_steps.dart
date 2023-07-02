a7be7b05abfe32beee1ba1af12521ce2ef3370aa
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-07-02 17:18:43 +0200

    prova sensore contapassi

--- APP/livechat/lib/screens/profile/components/circular_steps.dart
+++ APP/livechat/lib/screens/profile/components/circular_steps.dart
@@ -1,16 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:pedometer/pedometer.dart';
 import 'package:provider/provider.dart';
-
+import 'package:sensors/sensors.dart'; 
 import '../../../providers/settings_provider.dart';
 
-class CircularSteps extends StatelessWidget {
+class CircularSteps extends StatefulWidget {
   const CircularSteps({
     super.key,
   });
 
   @override
+  State<CircularSteps> createState() => _CircularStepsState();
+}
+
+class _CircularStepsState extends State<CircularSteps> {
+   late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    debugPrint(event.toString());
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    debugPrint(event.toString());
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    debugPrint('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    debugPrint(_status);
+  }
+
+  void onStepCountError(error) {
+    debugPrint('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() {
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      setState(() {
+        _steps = event.x.toString();
+      });
+    });
+
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    int steps = 4000;
+    int steps = int.tryParse(_steps) ?? 0;
     int goal = Provider.of<SettingsProvider>(context).settings.goalSteps;
 
     Color progressColor;
