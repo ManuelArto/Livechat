d5e51776c5ed7a2a8b0676be2c348c07cdf9bfa9
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-07-08 02:51:00 +0200

    Fix production mode per firebase credentials

--- SERVER/app/helpers/fb_images_upload.py
+++ SERVER/app/helpers/fb_images_upload.py
@@ -4,11 +4,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
 
-from app.config import Settings
+from app.config import settings
 
 cred = credentials.Certificate(
     path.join(path.dirname(__file__), "credentials.json")
-    if Settings.DEBUG_MODE
+    if settings.DEBUG_MODE
     else os.getenv("credentials_path")
 )
 firebase_admin.initialize_app(cred, {"storageBucket": "livechat-e7db8.appspot.com"})
