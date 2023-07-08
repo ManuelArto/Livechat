a925d9ec00e148b89ff2a260be8d98cd2706ffc0
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-07-08 02:07:54 +0200

    Fix logo

--- APP/livechat/lib/screens/friends/components/share_card.dart
+++ APP/livechat/lib/screens/friends/components/share_card.dart
@@ -27,7 +27,7 @@ class ShareCard extends StatelessWidget {
         subtitle: Text("livechat/${user.username}"),
         trailing: IconButton(
           onPressed: () {
-            Share.share("Discover Livechat, the ultimate communication app. Download now!\nhttps://www.facebook.com/watch/?v=298873375806313&extid=CL-UNK-UNK-UNK-AN_GK0T-GK1C&mibextid=2Rb1fB&ref=sharing");
+            Share.share("Discover Livechat, the ultimate communication app. Download now!\nhttps://example.com");
           },
           icon: const Icon(Icons.share_rounded),
         ),
